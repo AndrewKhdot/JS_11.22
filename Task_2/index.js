16ac4803c673aa9a18bbd6e2d718748b49ad0237
AndrewKhdot <khodotchuk.andrew@gmail.com>
2022-11-02 20:17:57 +0100

    [task_2]: finished the task

---
+++ Task_2/index.js
@@ -0,0 +1,33 @@
+let symbols = prompt('Enter a string no longer than three symbols');
+let num = prompt('Enter a positive integer less than 10');
+
+if(NumValidation(num) && StringValidation(symbols)) {
+    num = parseInt(num);
+    console.log(CreatResult(symbols, num));
+}
+else {
+    console.log('Incorrect input!');
+}
+
+function NumValidation (num) {
+    
+    return !isNaN(num) && (parseFloat(num) > 0) && (parseFloat(num) <= 10);
+
+}
+
+function StringValidation (str) {
+    return (str.length < 4) && (str.length > 0);
+
+}
+
+function CreatResult (val, num) {
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            result = result + val + ' ';
+        }
+        result = result + '\n';
+        
+    }
+    return result;
+}

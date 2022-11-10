4910d7ceb010f333a5f57fc5e180c0f208905131
AndrewKhdot <khodotchuk.andrew@gmail.com>
2022-11-10 13:40:49 +0100

    [task_2]: finished the task

--- task_2/task_2.js
+++ task_2/task_2.js
@@ -1,41 +1,30 @@
-let deb = createDebounceFunction(ceckfun, 10000);
-
-deb();
-
-setTimeout(deb, 1000);
-setTimeout(deb, 2000);
-setTimeout(deb, 3000);
-
-
 function createDebounceFunction (func, delay) {
     let counter = 0;
+    validation(func, delay);
     function callbackFunc () {
         counter++;
         let obj = {count: counter};
         initiator.call(obj);
         function initiator () {
-            console.log(`запуск инициатора ${this.count} ${new Date()}`);
             let count = this.count;
             function check () {
                 if(count - counter == 0){
                     func();
-                    console.log(`Конец фунуции ${count} ${new Date()}`);
-                }
-                else {
-                    console.log(`конец инициатора${count} ${new Date()}`);
                 }
             }   
             setTimeout(check, delay);
         }
-    function validation (fun, del) {
-
     }
-}
     callbackFunc.func = func;
     callbackFunc.delay = delay;
     return callbackFunc;
-}
+    function validation (fun, del) {
+        if(typeof fun != 'function') {
+            throw new Error;
+        }
+        if(isNaN(del) || !isFinite(del)) {
+            throw new Error;
+        }
 
-function ceckfun () {
-    console.log(100);
+    }
 }

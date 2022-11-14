a1353fd4020db322c95fca3453f8fdff717fb52b
AndrewKhdot <khodotchuk.andrew@gmail.com>
2022-11-14 15:40:51 +0100

    [task_3]: dont't work

--- task_3/task_3.js
+++ task_3/task_3.js
@@ -1,3 +1,14 @@
+class  RickAndMorty {
+    async getCharacter(x) {
+        let url = 'https://rickandmortyapi.com/api/character/';
+        url = url + x;
+        let response = await fetch(url);
+    
+        let json = await response.json();
+        return json;
+    }   
+}
+
 async function getChar() {
     let url = 'https://rickandmortyapi.com/api/character/1';
 
@@ -7,6 +18,9 @@ async function getChar() {
     console.log(text);
 }
 getChar();
+let rm = new RickAndMorty();
+rm.getCharacter(2);
+let ob = rm.getCharacter(2);
 /* fetch(url)
   .then(response => response.text())
   .then(text => console.log(text)); */
